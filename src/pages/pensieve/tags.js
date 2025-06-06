5032f5d4fdfc3d731ae614f9730d177f07499a96
lianafinaritra <hei.lianafinaritra@gmail.com>
2025-06-06 14:38:42 +0300

    fix: about component tag

--- src/pages/pensieve/tags.js
+++ src/pages/pensieve/tags.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Link, graphql } from 'gatsby';
-import kebabCase from 'lodash/kebabCase';
 import PropTypes from 'prop-types';
 import { Layout } from '@components';
 import styled from 'styled-components';
@@ -51,15 +50,6 @@ const TagsPage = ({
       </span>
 
       <h1>Tags</h1>
-      <ul className="fancy-list">
-        {group.map(tag => (
-          <li key={tag.fieldValue}>
-            <Link to={`/pensieve/tags/${kebabCase(tag.fieldValue)}/`}>
-              {tag.fieldValue} <span className="count">({tag.totalCount})</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
     </StyledTagsContainer>
   </Layout>
 );
